449cfd031ae060b51751f71d2d93e56986a82782
surajsadare <surajsadare9@gmail.com>
2024-02-13 10:33:33 +0530

    recipe list in progress

--- src/app/recipies/recipe-list/recipe-list.component.ts
+++ src/app/recipies/recipe-list/recipe-list.component.ts
@@ -3,8 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.scss']
+  styleUrls: ['./recipe-list.component.scss'],
 })
 export class RecipeListComponent {
-
+  recipes = [
+    {
+      name: 'Burger',
+      description: 'Try a once eat a daily',
+      imagePath:
+        'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg',
+    },
+  ];
 }
