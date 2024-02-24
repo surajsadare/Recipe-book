19e2a1e9ed60b49f204dac741c998b14faea90b9
surajsadare <surajsadare9@gmail.com>
2024-02-24 21:20:25 +0530

    basics set up

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -3,8 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
-  styleUrls: ['./shopping-list.component.scss']
+  styleUrls: ['./shopping-list.component.scss'],
 })
 export class ShoppingListComponent {
-
+  ingredients = [
+    { name: 'Apple', quantity: 5 },
+    { name: 'Grapes', quantity: 10 },
+  ];
 }
